f7bec919c74cdd2f451e1bffda6c7a3b5155f3be
designtechie <luckiekrrish@gmail.com>
2024-02-29 03:17:43 +0530

    29.02.2024

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import Pages from 'vite-plugin-pages';
+import generateSitemap from 'vite-plugin-pages-sitemap';
+import Sitemap from 'vite-plugin-sitemap';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+ plugins: [
+    react(),
+    Pages({
+      onRoutesGenerated: routes => (generateSitemap({ routes })),
+    }),
+    
+    Sitemap({
+     
+    }),
+ ],
+});
+
+
+
+
